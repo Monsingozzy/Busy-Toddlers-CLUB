553ad4503c985e1f55e30c8184f3612f8291e4b9
Monsingozzy <mohansinghyoyo88@gmail.com>
2023-03-05 10:25:17 +0530

    Intiated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="primary" expand="md">
+        <Container>
+          <Navbar.Collapse id="basic-navbar-nav" className="rnavs">
+            <div className="lnav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Help</Nav.Link>
+                <Nav.Link href="#link">About</Nav.Link>
+              </Nav>
+            </div>
+            <div className="rnav">
+              <Nav className="me-auto">
+                <Nav.Link href="#link">Views</Nav.Link>
+                <Nav.Link href="#link">Edits & Actions</Nav.Link>
+                <Nav.Link href="#link">Objectives</Nav.Link>
+              </Nav>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
